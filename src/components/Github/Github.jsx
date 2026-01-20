29f148cbc981e0db4825aae28c5f1444f074b93b
Bismit Wagle <bismitwagle@gmail.com>
2026-01-20 15:39:29 +0530

    Create and add Github component

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,26 @@
+    import React, { useEffect } from 'react'
+
+    function Github() {
+        // const [data,setData] = React.useState([])
+        // useEffect(()=> {
+        //     fetch('https://api.github.com/users/bismitw')
+        //     .then((response) => response.json())
+        // .then(data => {
+        //     setData(data)
+        //     console.log(data)
+        // })
+        // },[])
+
+    return (
+        <div className='text-center m-4 p-4 bg-gray-400 text-3xl text-white'>
+            Github Name: {data.name}
+            <img src={data.avatar_url} width={300} alt="" />
+            </div>
+    )
+    }
+
+    export default Github
+
+    export const githubInfoLoader = async () => {
+        const response  = await fetch('https://api.github.com/users/bismitw');
+    }
