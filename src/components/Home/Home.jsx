28d8311ea12ca093a0120466138a38fe9f99d1f9
Bismit Wagle <bismitwagle@gmail.com>
2026-01-20 12:27:02 +0530

    Add about us component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,13 +3,15 @@
 
         export default function Home() {
         return (
-        <div className="mx-auto w-full max-w-7xl">
+        <div className="mx-auto w-full max-w-7xl mb-20">
             <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
             <div className="relative z-10 max-w-7xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
                 <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
                 <h2 className="text-4xl font-bold sm:text-5xl">
                     Download Now
-                    <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
+                    <span className="hidden sm:block text-4xl">
+                    Lorem Ipsum
+                    </span>
                 </h2>
 
                 <Link
@@ -34,23 +36,11 @@
             <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
                 <img
                 className="w-96"
-                src="https://i.ibb.co/5BCcDYB/Remote2.png"
+                src="https://images.pexels.com/photos/30439897/pexels-photo-30439897.jpeg"
                 alt="image1"
                 />
             </div>
             </aside>
-
-            <div className="grid  place-items-center sm:mt-20">
-            <img
-                className="sm:w-96 w-48"
-                src="https://i.ibb.co/2M7rtLk/Remote1.png"
-                alt="image2"
-            />
-            </div>
-
-            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">
-            Lorem Ipsum Yojo
-            </h1>
         </div>
         );
         }
