f054014dffb6e7d9c22f2e41e5591070ebb30b6f
Bismit Wagle <bismitwagle@gmail.com>
2026-01-20 11:24:18 +0530

    Add Header component

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,13 @@ import App from './App.jsx'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
+import About from "./components/About/About.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home/>}/>
-
+      <Route path='about' element={<About/>}/>
     </Route>
   )
 )
