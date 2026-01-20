d80615394a26b165f87dc9f90eb4d564a5b218d4
Bismit Wagle <bismitwagle@gmail.com>
2026-01-20 12:47:35 +0530

    Add user component and dynamic route to user

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from "./components/About/About.jsx";
 import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -14,7 +15,9 @@ const router = createBrowserRouter(
       <Route path='' element={<Home/>}/>
       <Route path='about' element={<About/>}/>
       <Route path='contact' element={<Contact/>}/>
-
+      <Route path='user/' element={<User/>}>
+        <Route path=':userid' element={<User/>} />
+      </Route>
     </Route>
   )
 )
